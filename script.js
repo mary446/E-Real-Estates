cffbb7da5f1c7e32ca75d0c792fb722b307f6aca
mary446 <mnjenga446@gmail.com>
2025-03-13 08:14:48 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+$(function () {
+    let headerElem = $('header');
+    let logo = $('#logo');
+    let navMenu = $('#nav-menu');
+    let navToggle = $('#nav-toggle');
+
+    function subscribe() {
+        const email = document.querySelector('#newsletterModal input').value;
+        if (email.trim() === "") {
+          alert("Please enter an email.");
+        } else {
+          alert(`Thanks for subscribing, ${email}!`);
+          const modal = bootstrap.Modal.getInstance(document.getElementById('newsletterModal'));
+          modal.hide();
+        }
+      }
+      
+    // Initialize properties slider
+    $('#properties-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
+        nextArrow: '<a href="#" class="slick-arrow slick-next">next</a>',
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }
+        ]
+    });
+
+    // Mobile menu toggle
+    navToggle.on('click', () => {
+        navMenu.css('right', '0');
+    });
+
+    // Close mobile menu
+    $('#close-flyout').on('click', () => {
+        navMenu.css('right', '-100%');
+    });
+
+    // Close mobile menu when clicking outside
+    $(document).on('click', (e) => {
+        let target = $(e.target);
+        if (!(target.closest('#nav-toggle').length > 0 || target.closest('#nav-menu').length > 0)) {
+            navMenu.css('right', '-100%');
+        }
+    });
+
+    // Handle scroll effects
+    $(document).scroll(() => {
+        let scrollTop = $(document).scrollTop();
+
+        if (scrollTop > 0) {
+            navMenu.addClass('is-sticky');
+            logo.css('color', '#000');
+            headerElem.css('background', '#fff');
+            navToggle.css('border-color', '#000');
+            navToggle.find('.strip').css('background-color', '#000');
+        } else {
+            navMenu.removeClass('is-sticky');
+            logo.css('color', '#fff');
+            headerElem.css('background', 'transparent');
+            navToggle.css('bordre-color', '#fff');
+            navToggle.find('.strip').css('background-color', '#fff');
+        }
+
+        headerElem.css(scrollTop >= 200 ? 
+            {'padding': '0.5rem', 'box-shadow': '0 -4px 10px 1px #999'} : 
+            {'padding': '1rem 0', 'box-shadow': 'none'}
+        );
+    });
+
+   
+    $(document).trigger('scroll');
+});
